176ca021bc9ec0022393531f5d22e6dd7059ff45
RT <lemlem11217@gmail.com>
2019-07-18 18:22:53 -0700

    Generate Full Multiplication Table

---
+++ Quiz14.java
@@ -0,0 +1,18 @@
+
+public class Quiz14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int x=1;x<10;x++) {
+			for(int y=1;y<10;y++) {
+				System.out.printf("%2d",x*y);
+			    
+			}
+			   System.out.println();
+		}
+		
+		
+	}
+
+}
