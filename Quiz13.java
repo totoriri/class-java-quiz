7f0ca42299a4e2798b3b25ae7839106adef2426b
RT <lemlem11217@gmail.com>
2019-07-18 18:20:03 -0700

    print full pyramid.

---
+++ Quiz13.java
@@ -0,0 +1,25 @@
+
+public class Quiz13 {
+
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+     for (int i = 0; i < 5; i++) {
+    	 
+	       for (int j = 0; j < 5 - i; j++) {
+	          System.out.print(" ");
+	       }
+	       
+	      for (int k = 0; k <= i; k++) {
+	          System.out.print("* ");
+	      }
+	      
+      System.out.println();
+      
+     }
+   }
+
+}
+
+
